55f19ce40021d5b6e3aaef4d8336460b017a921e
Johannes Eriksson <johannes@stepzen.com>
2022-05-16 16:00:03 +0300

    Log when resolvers called

--- subgraphs/products/products.js
+++ subgraphs/products/products.js
@@ -23,33 +23,65 @@ const products = [
     { id: 'apollo-federation', sku: 'federation', package: '@apollo/federation', variation: "OSS" },
     { id: 'apollo-studio', sku: 'studio', package: '', variation: "platform" },
 ]
+
 const typeDefs = gql(readFileSync('./products.graphql', { encoding: 'utf-8' }));
+
+const bunyan = require('bunyan');
+
+// Imports the Google Cloud client library for Bunyan
+const {LoggingBunyan} = require('@google-cloud/logging-bunyan');
+
+// Creates a Bunyan Cloud Logging client
+const loggingBunyan = new LoggingBunyan();
+
+// Create a Bunyan logger that streams to Cloud Logging
+// Logs will be written to: "projects/YOUR_PROJECT_ID/logs/bunyan_log"
+const logger = bunyan.createLogger({
+  // The JSON payload of the log as it appears in Cloud Logging
+  // will contain "name": "my-service"
+  name: 'apollo-products',
+  streams: [
+    // Log to the console at 'info' and above
+    {stream: process.stdout, level: 'info'},
+    // And log to Cloud Logging, logging at 'info' and above
+    loggingBunyan.stream('info'),
+  ],
+});
+
+
 const resolvers = {
     Query: {
         allProducts: (_, args, context) => {
+            logger.info("Query > allProducts", args)
             return products;
         },
         product: (_, args, context) => {
+            logger.info("Query > product", args)
             return products.find(p => p.id == args.id);
         }
     },
     ProductItf: {
         __resolveType(obj, context, info){
+            logger.info("ProductItf > __resolveType", obj)
             return 'Product';
         },
     },
     Product: {
         variation: (reference) => {
+            logger.info("Product > variation", reference)
             if (reference.variation) return { id: reference.variation };
             return { id: products.find(p => p.id == reference.id).variation }
         },
         dimensions: () => {
+            logger.info("Product > dimensions")
             return { size: "1", weight: 1 }
         },
         createdBy: (reference) => {
+            logger.info("Product > createdBy", reference)
             return { email: 'support@apollographql.com', totalProductsCreated: 1337 }
         },
         __resolveReference: (reference) => {
+            logger.info("Product > __resolveReference", reference)
             if (reference.id) return products.find(p => p.id == reference.id);
             else if (reference.sku && reference.package) return products.find(p => p.sku == reference.sku && p.package == reference.package);
             else return { id: 'rover', package: '@apollo/rover', ...reference };
